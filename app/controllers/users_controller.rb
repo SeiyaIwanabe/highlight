adf5b1017df5626a6a8e4970f7938daff57e98b9
SeiyaIwanabe <mystory.i.seiya@gmail.com>
2020-09-13 05:06:18 +0900

    user-edit画面の実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,12 +4,18 @@ class UsersController < ApplicationController
   end
 
   def edit
+    @user = User.find(params[:id])
   end
 
   def update
+    if current_user.update(user_params)
+      redirect_to user_path(current_user), notice: "プロフィールを更新しました！"
+    else
+      render :edit
+    end
   end
 
   def user_params
-    params.require(:user).permit(:username, :email, :image)
+    params.require(:user).permit(:username, :email, :image, :introduction)
   end
 end
